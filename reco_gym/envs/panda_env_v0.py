afcb0256f4a8e33e2acb51b13198ce65a2b11a23
kiddyboots216 <kiddyboots216@berkeley.edu>
2019-06-13 19:03:42 -0700

    this should work but it doesn't

--- reco_gym/envs/panda_env_v0.py
+++ reco_gym/envs/panda_env_v0.py
@@ -8,14 +8,34 @@
 # Beta is the latent representation of response to actions (matrix P by K)
 # sigmoid(beta omega) is the ctr for each action
 import numpy as np
+import gym
+
 from numpy import array, diag, exp, matmul, mod
+from numpy.random.mtrand import RandomState
 from scipy.special import expit as sigmoid
-from .abstract import AbstractEnv, env_args, organic
-from .reco_env_v1 import RecoEnv1
+from gym.spaces import Discrete, Dict, Box
+
+from .session import OrganicSessions
+from .context import DefaultContext
+from .configuration import Configuration
+from .observation import Observation
+
+from .features.time import DefaultTimeGenerator
 
 # Default arguments for toy environment ------------------------------------
 
 # inherit most arguments from abstract class
+env_args = {
+    'max_time': 789,
+    'num_products': 10,
+    'num_users': 100,
+    'random_seed': np.random.randint(2 ** 31 - 1),
+    # Markov State Transition Probabilities.
+    'prob_leave_bandit': 0.01,
+    'prob_leave_organic': 0.01,
+    'prob_bandit_to_organic': 0.05,
+    'prob_organic_to_bandit': 0.25,
+}
 env_1_args = {
     **env_args,
     **{
@@ -41,9 +61,43 @@ stop = 2
 # Environment definition.
 # Amazon = Organic
 # Facebook = Bandit
-class PandaEnv0(RecoEnv1):
+class PandaEnv0(gym.Env):
     def __init__(self):
-        super().__init__()
+        gym.Env.__init__(self)
+
+    def init_gym(self, args):
+        self.config = Configuration(args)
+
+        # Defining Action Space.
+        self.action_space = Discrete(self.config.num_products)
+
+        # Defining Observation Space
+        self.observation_space = Box(
+            low = np.array([0, 0, 0]),
+            high = np.array([self.config.max_time,
+                            self.config.num_users,
+                            self.config.num_products]),
+            dtype = np.int32,
+            )
+        if 'time_generator' not in args:
+            self.time_generator = DefaultTimeGenerator(self.config)
+        else:
+            self.time_generator = self.config.time_generator
+
+        # Setting random seed for the first time.
+        self.reset_random_seed()
+
+        if 'agent' not in args:
+            self.agent = None
+        else:
+            self.agent = self.config.agent
+
+        # Setting any static parameters such as transition probabilities.
+        self.set_static_params()
+
+        # Set random seed for second time, ensures multiple epochs possible.
+        self.reset_random_seed()
+        self.reset()
 
     # @override
     def step(self, action_id):
@@ -99,7 +153,137 @@ class PandaEnv0(RecoEnv1):
             obs = np.array((self.current_time, self.current_user_id, self.product_view))
             done = True if self.state == stop else False
             return obs, rew, done, info
-        if self.state == stop:
-            obs = np.array((self.current_time, self.current_time, 0))
-            done = True
-            return obs, rew, done, info
+        # if self.state == stop:
+        obs = np.array((self.current_time, self.current_time, 0))
+        done = True
+        return obs, rew, done, info
+        # else:
+        #     return np.array
+
+    def set_static_params(self):
+        # Initialise the state transition matrix which is 3 by 3
+        # high level transitions between organic, bandit and leave.
+        self.state_transition = array([
+            [0, self.config.prob_organic_to_bandit, self.config.prob_leave_organic],
+            [self.config.prob_bandit_to_organic, 0, self.config.prob_leave_organic],
+            [0.0, 0.0, 1.]
+        ])
+
+        self.state_transition[0, 0] = 1 - sum(self.state_transition[0, :])
+        self.state_transition[1, 1] = 1 - sum(self.state_transition[1, :])
+
+        # Initialise Gamma for all products (Organic).
+        self.Gamma = self.rng.normal(
+            size = (self.config.num_products, self.config.K)
+        )
+
+        # Initialise mu_organic.
+        self.mu_organic = self.rng.normal(
+            0, self.config.sigma_mu_organic,
+            size = (self.config.num_products)
+        )
+
+        # Initialise beta, mu_bandit for all products (Bandit).
+        self.generate_beta(self.config.number_of_flips)
+
+    def reset_random_seed(self, epoch = 0):
+        # Initialize Random State.
+        assert (self.config.random_seed is not None)
+        self.rng = RandomState(self.config.random_seed + epoch)
+    
+    # Create a new user.
+    def reset(self):
+        self.first_step = True
+        self.state = organic  # Manually set first state as Organic.
+
+        self.time_generator.reset()
+        if self.agent:
+            self.agent.reset()
+
+        self.current_time = self.time_generator.new_time()
+        self.current_user_id = np.random.randint(self.config.num_users)
+
+        # Record number of times each product seen for static policy calculation.
+        self.organic_views = np.zeros(self.config.num_products)
+        self.omega = self.rng.normal(
+            0, self.config.sigma_omega_initial, size = (self.config.K, 1)
+        )
+
+    # Update user state to one of (organic, bandit, leave) and their omega (latent factor).
+    def update_state(self):
+        self.state = self.rng.choice(3, p = self.state_transition[self.state, :])
+        assert (hasattr(self, 'time_generator'))
+        old_time = self.current_time
+        self.current_time = self.time_generator.new_time()
+        time_delta = self.current_time - old_time
+        omega_k = 1 if time_delta == 0 else time_delta
+
+        # And update omega.
+        if self.config.change_omega_for_bandits or self.state == organic:
+            self.omega = self.rng.normal(
+                self.omega,
+                self.config.sigma_omega * omega_k, size = (self.config.K, 1)
+            )
+
+    # Sample a click as response to recommendation when user in bandit state
+    # click ~ Bernoulli().
+    def draw_click(self, recommendation):
+        # Personalised CTR for every recommended product.
+        ctr = ff(matmul(self.beta, self.omega)[:, 0] + self.mu_bandit)
+        click = self.rng.choice(
+            [0, 1],
+            p = [1 - ctr[recommendation], ctr[recommendation]]
+        )
+        return click
+
+    # Sample the next organic product view.
+    def update_product_view(self):
+        log_uprob = matmul(self.Gamma, self.omega)[:, 0] + self.mu_organic
+        log_uprob = log_uprob - max(log_uprob)
+        uprob = exp(log_uprob)
+        self.product_view = int(
+            self.rng.choice(
+                self.config.num_products,
+                p = uprob / sum(uprob)
+            )
+        )
+
+    def generate_beta(self, number_of_flips):
+        """Create Beta by flipping Gamma, but flips are between similar items only"""
+        if number_of_flips == 0:
+            self.beta = self.Gamma
+            self.mu_bandit = self.mu_organic
+            return
+        P, K = self.Gamma.shape
+        index = list(range(P))
+
+        prod_cov = matmul(self.Gamma, self.Gamma.T)
+        prod_cov = prod_cov - diag(
+            diag(prod_cov))  # We are always most correlated with ourselves so remove the diagonal.
+
+        prod_cov_flat = prod_cov.flatten()
+
+        already_used = dict()
+        flips = 0
+        pcs = prod_cov_flat.argsort()[::-1]  # Find the most correlated entries
+        for ii, jj in [(int(p / P), mod(p, P)) for p in pcs]:  # Convert flat indexes to 2d indexes
+            # Do flips between the most correlated entries
+            # provided neither the row or col were used before.
+            if not (ii in already_used or jj in already_used):
+                index[ii] = jj  # Do a flip.
+                index[jj] = ii
+                already_used[ii] = True  # Mark as dirty.
+                already_used[jj] = True
+                flips += 1
+
+                if flips == number_of_flips:
+                    self.beta = self.Gamma[index, :]
+                    self.mu_bandit = self.mu_organic[index]
+                    return
+
+        self.beta = self.Gamma[index, :]
+        self.mu_bandit = self.mu_organic[index]
+
+if __name__ == "__main__":
+    env = PandaEnv0()
+    env.init_gym(env_1_args)
